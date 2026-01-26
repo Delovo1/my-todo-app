d580d1a39eb0bebb732c5eacf786ce9ced1f6fd3
DeLoVo1 <maxsim.0981@gmail.com>
2026-01-26 19:23:05 +0300

    added sort

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Main = ({ tasks, setTasks }) => {
   const [text, setText] = useState("");
   const [red, setRed] = useState(false);
-
+  const [where, setWhere] = useState(true);
   const toggleTodo = (id) => {
     setTasks((prev) =>
       prev.map((task) =>
@@ -72,31 +72,58 @@ const Main = ({ tasks, setTasks }) => {
       </div>
 
       <div className="todolist">
-        {[...tasks]
-          .sort((a, b) => a.completed - b.completed)
-          .map((task) => (
-            <div
-              key={task.id}
-              className={`task 
+        <img
+          className={`sorted ${tasks.length === 0 ? "off" : "on"}`}
+          src={`${process.env.PUBLIC_URL}/images/${
+            where ? "sortdown" : "sortup"
+          }.png`}
+          alt="sorted button"
+          title="sort your tasks!"
+          onClick={() => {
+            const countcompl = tasks.filter((task) => task.completed).length;
+            if (
+              countcompl === 0 ||
+              countcompl === tasks.length ||
+              tasks.length === 1
+            ) {
+              return;
+            }
+            if (where) {
+              setTasks((prev) =>
+                [...prev].sort((a, b) => a.completed - b.completed)
+              );
+              setWhere(false);
+              return;
+            }
+            setTasks((prev) =>
+              [...prev].sort((a, b) => b.completed - a.completed)
+            );
+            setWhere(true);
+          }}
+        />
+        {[...tasks].map((task) => (
+          <div
+            key={task.id}
+            className={`task 
                 ${task.removing ? "removing" : ""} 
                 ${task.completed ? "completed" : ""}
               `}
-            >
-              <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => toggleTodo(task.id)}
-              />
+          >
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTodo(task.id)}
+            />
 
-              <label>{task.text}</label>
+            <label>{task.text}</label>
 
-              <img
-                src={`${process.env.PUBLIC_URL}/images/Union.png`}
-                alt="delete"
-                onClick={() => removeTask(task.id)}
-              />
-            </div>
-          ))}
+            <img
+              src={`${process.env.PUBLIC_URL}/images/Union.png`}
+              alt="delete"
+              onClick={() => removeTask(task.id)}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
